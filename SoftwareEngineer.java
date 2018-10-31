a7344d8f52864e4cbcff70c3f6dcc06b104ecfa8
yndprasad <yndprasad901@gmail.com>
2018-10-31 10:24:24 +0530

    Added new file for new projects

---
+++ SoftwareEngineer.java
@@ -0,0 +1,12 @@
+
+public class SoftwareEngineer extends Employee
+{
+	private String projectName;
+
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}	
+}
